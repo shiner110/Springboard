5f054f7520077bfe54571b36a4c26258da719e19
SHAGHAYEGH ROSHAN <shaghayeghroshan@SHAGHAYEGHs-MacBook-Air.local>
2022-03-02 07:34:45 -0700

    update

---
+++ Capstone_3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Time analysis of TSA throughput 2019-2022',
+    author='Shine Roshan',
+    license='MIT',
+)
